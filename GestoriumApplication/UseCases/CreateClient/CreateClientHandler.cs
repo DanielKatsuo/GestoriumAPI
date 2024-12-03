3d530c0f74e47a4398ff32334d11a8443515f0ef
Daniel Silva <88464213+DanielKatsuo@users.noreply.github.com>
2024-12-03 01:05:38 -0300

    feat: Refatora CreateClientHandler e adiciona IClientRepository
    As mudanças no arquivo `CreateClientHandler.cs` incluem a adição de várias importações de namespaces, como `AutoMapper`, `GestoriumAPI.Domain.Entities`, `GestoriumAPI.Domain.Interfaces` e `MediatR`. A classe `CreateClientHandler` foi modificada de `internal` para `public sealed` e agora implementa a interface `IRequestHandler<CreateClientRequest, CreateClientResponse>`. O construtor da classe foi alterado para receber instâncias de `IClientRepository`, `IUnitOfWork` e `IMapper` como parâmetros. Foram adicionados campos privados para armazenar essas instâncias. Um método `Handle` assíncrono foi adicionado para lidar com a criação de um cliente, mapeando a solicitação para um objeto `Client`, adicionando-o ao repositório e confirmando a transação, retornando uma resposta mapeada.
    
    No arquivo `IClientRepository.cs`, foi adicionada uma nova interface `IClientRepository` que herda de `IBaseRepository<Client>`. Esta interface está localizada no namespace `GestoriumAPI.Domain.Interfaces` e inclui as importações necessárias.

--- GestoriumApplication/UseCases/CreateClient/CreateClientHandler.cs
+++ GestoriumApplication/UseCases/CreateClient/CreateClientHandler.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using AutoMapper;
+using GestoriumAPI.Domain.Entities;
+using GestoriumAPI.Domain.Interfaces;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,18 @@ using System.Threading.Tasks;
 
 namespace GestoriumAPI.Application.UseCases.CreateClient
 {
-	internal class CreateClientHandler
+	public sealed class CreateClientHandler(IClientRepository clientRepository, IUnitOfWork unitOfWork, IMapper mapper) : IRequestHandler<CreateClientRequest, CreateClientResponse>
 	{
+		private readonly IClientRepository _clientRepository = clientRepository;
+		private readonly IMapper _mapper = mapper;
+		private readonly IUnitOfWork _unitOfWork = unitOfWork;
+
+		public async Task<CreateClientResponse> Handle(CreateClientRequest request, CancellationToken cancellationToken)
+		{
+			var user = _mapper.Map<Client>(request);
+			await _clientRepository.AddAsync(user);
+			await _unitOfWork.CommitAsync();
+			return _mapper.Map<CreateClientResponse>(user);
+		}
 	}
 }
---
+++ GestoriumDomain/Interfaces/IClientRepository.cs
@@ -0,0 +1,13 @@
+﻿using GestoriumAPI.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestoriumAPI.Domain.Interfaces
+{
+	public interface IClientRepository : IBaseRepository<Client>
+	{
+	}
+}
